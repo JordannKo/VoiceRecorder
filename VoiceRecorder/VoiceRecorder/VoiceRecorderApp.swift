4716f8b4b3a99753763a6203eb5536f30dd6215c
JordannKo <jordannn0023@gmail.com>
2025-08-17 21:25:12 +0900

    create project

---
+++ VoiceRecorder/VoiceRecorder/VoiceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderApp.swift
+//  VoiceRecorder
+//
+//  Created by JordannKo on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecorderApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
